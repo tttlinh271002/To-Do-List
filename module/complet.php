25cfe9cb58e82c37b5953715463d69f7e9836069
Hoàng Thảo Linh <84242151+tttlinh271002@users.noreply.github.com>
2023-06-18 21:54:49 +0700

    to-do-list

--- module/complet.php
+++ module/complet.php
@@ -1,11 +1,11 @@
 <?php
-include 'database.php';
+include '../database.php';
 
 if (isset($_GET["id"])) {
 	$id = $_GET["id"];
 	$sql = "UPDATE `list` SET `complet` = '1' WHERE `list`.`id` = $id ";
 	if (mysqli_query($conn , $sql)) {
-		header("Location: home.php?complet-success"); 
+		header("Location: ../home.php?complet-success"); 
 	}
 
 	else{
