9eb032fbfdff19f282967b81cee77293fb58b9ba
Hoàng Thảo Linh <84242151+tttlinh271002@users.noreply.github.com>
2023-06-21 15:14:50 +0700

    to-do-list

--- list.sql
+++ list.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Jun 18, 2023 at 07:10 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- Host: localhost:3306
+-- Generation Time: Jun 21, 2023 at 03:14 PM
+-- Server version: 5.7.41-cll-lve
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `todolist`
+-- Database: `wppfafrx_todolist`
 --
 
 -- --------------------------------------------------------
@@ -32,7 +32,7 @@ CREATE TABLE `list` (
   `title` varchar(500) NOT NULL,
   `description` varchar(10000) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `date` date NOT NULL,
+  `odate` date NOT NULL,
   `complet` int(5) NOT NULL,
   `piority` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -41,10 +41,11 @@ CREATE TABLE `list` (
 -- Dumping data for table `list`
 --
 
-INSERT INTO `list` (`id`, `title`, `description`, `user_id`, `date`, `complet`, `piority`) VALUES
-(42, 'Do homework', 'hard-working', 1, '2023-06-18', 0, 0),
-(43, 'Learing Math', 'hard working', 1, '2023-06-20', 0, 2),
-(44, 'Learning English', 'hard working', 1, '2023-06-19', 1, 1);
+INSERT INTO `list` (`id`, `title`, `description`, `user_id`, `odate`, `complet`, `piority`) VALUES
+(50, 'Blockchain', 'har-working', 1, '2023-06-21', 0, 1),
+(51, 'Math', 'study hard', 1, '2023-06-24', 0, 0),
+(52, 'Machine Learning', 'study hard', 1, '2023-06-27', 0, 2),
+(53, 'Data Mining', 'hard-working', 1, '2023-06-22', 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -64,7 +65,7 @@ ALTER TABLE `list`
 -- AUTO_INCREMENT for table `list`
 --
 ALTER TABLE `list`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
